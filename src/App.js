a8a08644f178fb85d51577efe023672886fecfe3
Cyril Gouv <hello@cyrilgouv.com>
2022-04-14 11:17:27 +0200

    init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { text } from './data'
+
+import './App.css'
 
 function App() {
+  const [data, setData] = useState([])
+  const [value, setValue] = useState(0)
+
+
+  const handleParagraph = e => {
+
+    const amount = parseInt(e.target.value)
+
+    if (amount >= 0 && amount <= text.length) {
+      setValue(amount)
+      setData(text.slice(0, amount))
+    }
+      
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app__wrapper">
+        <div className="app__header">
+          <h1 className="app__title">Lorem Ipsum Generator</h1>
+          <form className="app__form">
+            <div>
+              <label htmlFor="paragraph">Paragraphs:</label>
+              <input type="number" name="paragraph" className="app__input" min="0" max={ text.length } onChange={ handleParagraph } value={ value } />
+            </div>
+          </form>
+        </div>
+
+        <div className="app__body">
+          
+            { data.map((para, idx) => {
+              return <p key={ idx }>{ para }</p>
+            }) }
+          
+        </div>
+      </div>
     </div>
   );
 }
